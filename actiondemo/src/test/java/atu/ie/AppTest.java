ca9cc8c3f909ccad2953378cd8aea39899e03bb7
Tony Thomas - STUDENT <L00171045@atu.ie>
2022-10-26 10:48:55 +0100

    Initial commit@

---
+++ actiondemo/src/test/java/atu/ie/AppTest.java
@@ -0,0 +1,20 @@
+package atu.ie;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
